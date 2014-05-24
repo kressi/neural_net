c3be6a793a69e7e7574a9ea8a571cb6feacc46cb
kressi <mkressibucher@hotmail.com>
2014-05-24 22:56:39 +0200

    travis.yml updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PatternRecognition",
+    version="0.1",
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "scipy"
+    ]
+)
