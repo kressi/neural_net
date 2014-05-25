4f40255641663f88212e080197b86498d6cc8860
kressi <mkressibucher@hotmail.com>
2014-05-25 14:35:37 +0200

    flask dummy added

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="PatternRecognition",
-    version="0.1",
-    install_requires=[
-        "numpy",
-        "scikit-learn",
-        "scipy"
-    ]
+  name="PatternRecognition",
+  version="0.1",
+  install_requires=[
+    "numpy",
+    "scikit-learn",
+    "scipy",
+    "flask"
+  ]
 )
