44671ab54ff0c23288589c4808049127fa18fb88
kressi <mkressibucher@hotmail.com>
2014-05-25 20:41:06 +0200

    post request fixed

--- pattern_recognition/api.py
+++ pattern_recognition/api.py
@@ -1,26 +1,29 @@
 import os
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
+@app.route("/api")
 @app.route("/")
 def api():
   return "api"
 
-@app.route("/api")
-return api()
-
 @app.route("/train-mnist")
-def train()
+def train_mnist():
   return "training neural net with mnist training data"
 
-@app.route("/train", method='POST')
+@app.route("/train", methods=["POST"])
+def train():
   return "train neural net with single letter"
 
 @app.route("/reset")
+def reset():
   return "neural net reset"
 
-@app.route("/recognize", method='POST')
+@app.route("/recognize", methods=["POST"])
+def recognize():
+  f = request.files['the_file']
   return "recognize pattern"
 
 if __name__ == "__main__":
