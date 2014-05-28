3ec98f43f682d33a78803d66017488b4c7ae0acb
Michael Kressibucher <mkressibucher@hotmail.com>
2014-05-28 10:38:50 +0200

    flask-cors added

--- pattern_recognition/api.py
+++ pattern_recognition/api.py
@@ -7,6 +7,7 @@
 #### Libraries
 # Standard library
 import os
+import flask-cors
 import simplejson as json
 from flask import Flask
 from flask import request
@@ -18,12 +19,14 @@ from redis_connector import redis
 
 
 app = Flask(__name__)
+app.config['CORS_ORIGINS'] = ['http://kressi.github.io']
 
 @app.route("/")
 def index():
   return 'Github project of <a href="https://github.com/kressi/neural_net">neural_net</a>'
 
 @app.route("/api")
+@cross_origin(headers=['Content-Type'])
 def api():
   pretty_api=json.dumps({'train_with_mnist': 'http://neural-net.herokuapp.com/train-mnist',
                          'train': 'http://neural-net.herokuapp.com/train',
@@ -33,19 +36,23 @@ def api():
   return Response(pretty_api, mimetype='application/json')
 
 @app.route("/train-mnist")
+@cross_origin(headers=['Content-Type'])
 def train_mnist():
   return net_runner.train_mnist()
 
 @app.route("/train", methods=["POST"])
+@cross_origin(headers=['Content-Type'])
 def train():
   return "train neural net with single letter - not implemented yet"
 
 @app.route("/reset")
+@cross_origin(headers=['Content-Type'])
 def reset():
   count=redis.delete('nn-status')
   return "net reset, %d records deleted from redis" % count
 
 @app.route("/recognize-pattern", methods=["POST"])
+@cross_origin(headers=['Content-Type'])
 def recognize():
   pattern = request.get_json()['pattern']
   result = net_runner.recognize_pattern(pattern)
